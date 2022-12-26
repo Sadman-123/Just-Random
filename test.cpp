e19eb390742d1427680a96e4ca762c50da5e0efa
Sadman26 <fareless.ranger@gmail.com>
2022-12-27 04:07:07 +0600

    MERGED

--- test.cpp
+++ test.cpp
@@ -1,5 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
+void loop(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<i<<endl;
+    }
+}
 int sum(int x){
     return x+x;
 }
@@ -13,6 +20,7 @@ int main()
     cout<<'Hello Sadman';
     cout<<'Hello Sakib';
     show();
+    loop(5);
     cout<<sum(5);
     return 0;
 }
